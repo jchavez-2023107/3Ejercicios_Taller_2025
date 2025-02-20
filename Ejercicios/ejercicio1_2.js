d9536c2941cf50f7eaaadad51457bbdeade73227
jchavez-2023107 <jchavez-2023107@kinal.edu.gt>
2025-02-19 22:04:43 -0600

    Se arregla el ejercicio 1.2

---
+++ Ejercicios/ejercicio1_2.js
@@ -0,0 +1,18 @@
+// Definimos el carrito con productos
+const carrito = [
+    { nombre: "Laptop", precio: 12000, cantidad: 27 },
+    { nombre: "Mouse", precio: 25, cantidad: 30 },
+    { nombre: "Teclado", precio: 50, cantidad: 27 }
+];
+
+// Calculamos el total por producto y el costo total del carrito
+let costoTotal = 0;
+
+console.log("Lista de productos en el carrito:");
+carrito.forEach(producto => {
+    const totalProducto = producto.precio * producto.cantidad;
+    console.log(`- ${producto.nombre}: ${producto.cantidad} x $${producto.precio} = $${totalProducto}`);
+    costoTotal += totalProducto;
+});
+
+console.log(`\nCosto total del carrito: $${costoTotal}`);
